5740f4d22e68d6c5942d0fc1ecb0c208c0f18c73
jothinathan423 <jothinathannagarajan@gmail.com>
2025-04-03 20:32:06 +0530

    headslittile

--- routes/Api.js
+++ routes/Api.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Admin = require('../model/Admin');
+const Head = require('../model/Head');
 
 
 
@@ -20,6 +21,55 @@ router.post('/adminregister', async (req, res) => {
     }
     catch (err) {
         res.status(500).json('serve error');
+    }
+})
+
+
+
+
+
+//insert heads
+router.post('/headinsert', async (req, res) => {
+    try {
+        const { id, name, email, password, designation } = req.body;
+        const userexist = await Head.findOne({ id: id });
+        if (userexist) {
+            res.status(400).json('Already Exist');
+        }
+        const newHead = new Head({ id, name, email, password, designation });
+        await newHead.save();
+        res.status(201).json({message:'head inserted successfully', user: newHead});
+    }
+    catch (err) {
+        res.status(500).json('server error');
+    }
+})
+
+//get all heads
+router.get('/getallheads', async (req, res) => {
+    try {
+        const heads = await Head.find();
+        res.status(200).json(heads);
+    }
+    catch (err) {
+        res.status(500).json('server error');
+    }
+})
+
+
+//remove heads by the id
+router.delete('/deleteheadbyid', async (req, res) => {
+    try {
+        const { id } = req.body;
+        const head = await Head.findOneAndDelete({ id: id });
+        if (!head) {
+            return res.status(404).json('head not found');
+        } else {
+            res.status(200).json({ message: 'head removed successfully', head });
+        }
+    }
+    catch (err) {
+        res.status(500).json('server error');
     }
 })
 
