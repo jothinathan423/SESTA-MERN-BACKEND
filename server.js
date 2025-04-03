ea76b87d66cf4fbcfbf81646ba806a10e5ae2582
jothinathan423 <jothinathannagarajan@gmail.com>
2025-04-03 19:08:50 +0530

    starting1

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const connectDb = require('./db/config');
 const cors = require('cors');
 const dotenv = require('dotenv');
-
+dotenv.config();
 
 const app = express();
+connectDb();
 app.use(cors());
 app.use(express.json());
+
+app.use('/api', require('./routes/Api'));
 
 const port = 5000;
 app.listen(port, () => {
