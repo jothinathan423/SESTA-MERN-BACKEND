627de0be0e163754629c0037b56a6ee5d1eb780f
Preethi2515 <preethimallika6@gmail.om>
2025-04-04 20:28:19 +0530

    inserted ca

--- routes/Api.js
+++ routes/Api.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Admin = require('../model/Admin');
-
-
-
+const Ca = require('../model/ca');
 
 //admin register
 router.post('/adminregister', async (req, res) => {
@@ -22,5 +20,25 @@ router.post('/adminregister', async (req, res) => {
         res.status(500).json('serve error');
     }
 })
+
+//insert ca's
+router.post('/insertca',async (req,res) =>{
+    try{
+        const { name, department , section, email } = req.body;
+        const userexist = await Ca.findOne({email: email});
+        if(userexist){
+            res.status(400).json('Class Advisor already exists');
+        }
+        const newCa = new Ca({name, department , section, email});
+        await newCa.save();
+        res.status(201).json({message: 'Class Advisor inserted Successfully',user: newCa });
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).json({ error: err.message });
+    }
+})
+
+//view ca's
 
 module.exports = router;
