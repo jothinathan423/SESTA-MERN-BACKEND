328f7a0dc9e5aebb300f1b74d9ef5fefc0d7164f
Rubigasekar <rubikasekar123@gmail.com>
2025-04-08 20:08:47 +0530

    hoddone

--- routes/Api.js
+++ routes/Api.js
@@ -4,9 +4,11 @@ const router = express.Router();
 // Import sub-route files
 const adminRoutes = require('./AdminApi');
 const admincaInsert = require('./AdminCa');
+const adminHodInsert = require('./AdminHod');
 // Use those routes
 router.use('/admin', adminRoutes);
 router.use('/ca', admincaInsert);
+router.use('/Hod',adminHodInsert);
   // example
 
 module.exports = router;
