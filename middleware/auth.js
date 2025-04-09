c3ad177f99b38b06f3d61ff2559926e74383e507
Preethi2515 <preethimallika6@gmail.om>
2025-04-09 11:43:52 +0530

    CA done

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const { JsonWebTokenError } = require('jsonwebtoken');
 
 const auth = (req, res, next) => {
     try {
--- routes/AdminCa.js
+++ routes/AdminCa.js
@@ -2,13 +2,42 @@ const express = require('express');
 const router = express.Router();
 const ClassAdvisor = require('../models/ca');
 const auth = require('../middleware/auth');
+const jwt  = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const SECRET_KEY = process.env.JWT_sECRET;
+//Class advisor login
+router.post('/calogin',async (req, res) => {
+    const {email, password} = req.body;
+    try {
+        const ca = await ClassAdvisor.findOne({email});
+        if(!ca) 
+            return res.status(401).json({message: 'Invalid'});
 
-//  Insert Class Advisor (Admin only)
+        const isMatch = await bcrypt.compare(password, ca.password);
+        if(!isMatch)
+            return res.status(401).json({message: 'Invalid pass'});
+        const token = jwt.sign({email:ca.email, id:ca.id}, SECRET_KEY, {expiresIn:'10m'});
+        res.status(200).json({message:'Login Successful',token});
+    }catch (err){
+        res.status(500).json({error:err.message});
+    }
+});
+
+
+// Insert Class Advisor (Admin only)
 router.post('/insert', auth, async (req, res) => {
     try {
-        const { name, department, section, email,password } = req.body;
+        const { name, department, section, email, password } = req.body;
 
-        const newCA = new ClassAdvisor({ name, department, section, email,password });
+        const existingCA = await ClassAdvisor.findOne({ email });
+        if (existingCA) {
+            return res.status(400).json({ message: 'Class Advisor already exists' });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newCA = new ClassAdvisor({ name,department,section,email,password: hashedPassword});
+
         await newCA.save();
 
         res.status(201).json({ message: 'Class Advisor inserted successfully', ca: newCA });
@@ -16,6 +45,7 @@ router.post('/insert', auth, async (req, res) => {
         res.status(500).json({ message: 'Failed to insert Class Advisor', error: error.message });
     }
 });
+
 
 //  Delete Class Advisor (by email, Admin only)
 router.delete('/delete/:email', auth, async (req, res) => {
@@ -32,6 +62,7 @@ router.delete('/delete/:email', auth, async (req, res) => {
         res.status(500).json({ message: 'Failed to delete Class Advisor', error: error.message });
     }
 });
+//view Class Advisors
 router.get('/viewCas',auth, async (req,res) => {
     try{
         const cas = await ClassAdvisor.find();
